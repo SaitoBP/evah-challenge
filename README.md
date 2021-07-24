e0ac0646cefbe0aee16ea1064e4a8816c9f79e40
Gabriel <gabrielpereira050201@hotmail.com>
2021-07-24 17:03:00 -0300

    ❗ Created Client folder

---
+++ README.md
@@ -0,0 +1,3 @@
+# Evah Challenge
+
+Uma aplicação fullstack simples feita como candidatura de vaga na [Evah](https://www.evah.io/)
---
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <section>
+      <h1>Landing Page</h1>
+    </section>
+  )
+}
+
+export default LandingPage
